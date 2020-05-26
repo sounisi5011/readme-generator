e5d98ac05bc3a1187cb3496a1d4f849484fec7ba
sounisi5011 <sounisi5011@users.noreply.github.com>
2020-05-26 18:16:44 +0900

    🔧 Add "tests" directory for placing test files

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   preset: 'ts-jest',
   coverageDirectory: 'coverage',
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tests/tsconfig.json',
+    },
+  },
   testEnvironment: 'node',
+  testMatch: ['<rootDir>/tests/**/*.ts'],
 };
