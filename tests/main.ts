c30c2821db7c62faceff649643de0e94c7bb1c6a
sounisi5011 <sounisi5011@users.noreply.github.com>
2020-05-28 01:42:48 +0900

    ✅ Add tests for template filters: repoBrowseURL

--- tests/main.ts
+++ tests/main.ts
@@ -654,6 +654,101 @@ describe('cli', () => {
                 ].join('\n'),
             );
         });
+
+        it('repoBrowseURL', async () => {
+            const cwd = await createFixturesDir('cli/filter/repoBrowseURL');
+            await writeFilesAsync(cwd, {
+                'package.json': {
+                    version: '1.4.2',
+                    repository: 'https://github.com/example/repo.git',
+                },
+                [DEFAULT_TEMPLATE_NAME]: [
+                    `{{ './package.json' | repoBrowseURL }}`,
+                    `{{ '../package.json' | repoBrowseURL }}`,
+                    `{{ '/package.json' | repoBrowseURL }}`,
+                    `{{ 'package.json' | repoBrowseURL }}`,
+                    ``,
+                    `{{ '/package.json' | repoBrowseURL(committish='COMMIT-ISH') }}`,
+                    `{{ '/package.json' | repoBrowseURL(commit='4626dfa') }}`,
+                    `{{ '/package.json' | repoBrowseURL(branch='gh-pages') }}`,
+                    `{{ '/package.json' | repoBrowseURL(tag='foo') }}`,
+                    ``,
+                    `* < committish`,
+                    `{{ '/package.json' | repoBrowseURL(committish='COMMIT-ISH', commit='4626dfa', branch='gh-pages', tag='foo', other='???') }}`,
+                    `{{ '/package.json' | repoBrowseURL(commit='4626dfa', branch='gh-pages', tag='foo', other='???', committish='COMMIT-ISH') }}`,
+                    `{{ '/package.json' | repoBrowseURL(commit='4626dfa', branch='gh-pages', committish='COMMIT-ISH', tag='foo', other='???') }}`,
+                    ``,
+                    `* < commit < committish`,
+                    `{{ '/package.json' | repoBrowseURL(commit='4626dfa', branch='gh-pages', tag='foo', other='???') }}`,
+                    `{{ '/package.json' | repoBrowseURL(branch='gh-pages', tag='foo', other='???', commit='4626dfa') }}`,
+                    `{{ '/package.json' | repoBrowseURL(branch='gh-pages', commit='4626dfa', tag='foo', other='???') }}`,
+                    ``,
+                    `* < branch < commit < committish`,
+                    `{{ '/package.json' | repoBrowseURL(branch='gh-pages', tag='foo', other='???') }}`,
+                    `{{ '/package.json' | repoBrowseURL(tag='foo', other='???', branch='gh-pages') }}`,
+                    `{{ '/package.json' | repoBrowseURL(tag='foo', branch='gh-pages', other='???') }}`,
+                    ``,
+                    `tag < branch < commit < committish`,
+                    `{{ '/package.json' | repoBrowseURL(tag='foo', other='???') }}`,
+                    `{{ '/package.json' | repoBrowseURL(other='???', tag='foo') }}`,
+                    ``,
+                    `other options are ignored`,
+                    `{{ '/package.json' | repoBrowseURL(other='???') }}`,
+                    `{{ '/package.json' | repoBrowseURL() }}`,
+                    `{{ '/package.json' | repoBrowseURL }}`,
+                ],
+            });
+
+            expect(await execCli(cwd, [])).toMatchObject({
+                exitCode: 0,
+                stdout: '',
+            });
+            await expect(
+                readFileAsync(path.join(cwd, 'README.md'), 'utf8'),
+            ).resolves.toBe(
+                [
+                    `https://github.com/example/repo/tree/v1.4.2/${path.relative(
+                        projectRootDirpath,
+                        cwd,
+                    )}/package.json`,
+                    `https://github.com/example/repo/tree/v1.4.2/${path.relative(
+                        projectRootDirpath,
+                        path.dirname(cwd),
+                    )}/package.json`,
+                    `https://github.com/example/repo/tree/v1.4.2/package.json`,
+                    `https://github.com/example/repo/tree/v1.4.2/package.json`,
+                    ``,
+                    `https://github.com/example/repo/tree/COMMIT-ISH/package.json`,
+                    `https://github.com/example/repo/tree/4626dfa/package.json`,
+                    `https://github.com/example/repo/tree/gh-pages/package.json`,
+                    `https://github.com/example/repo/tree/foo/package.json`,
+                    ``,
+                    `* < committish`,
+                    `https://github.com/example/repo/tree/COMMIT-ISH/package.json`,
+                    `https://github.com/example/repo/tree/COMMIT-ISH/package.json`,
+                    `https://github.com/example/repo/tree/COMMIT-ISH/package.json`,
+                    ``,
+                    `* < commit < committish`,
+                    `https://github.com/example/repo/tree/4626dfa/package.json`,
+                    `https://github.com/example/repo/tree/4626dfa/package.json`,
+                    `https://github.com/example/repo/tree/4626dfa/package.json`,
+                    ``,
+                    `* < branch < commit < committish`,
+                    `https://github.com/example/repo/tree/gh-pages/package.json`,
+                    `https://github.com/example/repo/tree/gh-pages/package.json`,
+                    `https://github.com/example/repo/tree/gh-pages/package.json`,
+                    ``,
+                    `tag < branch < commit < committish`,
+                    `https://github.com/example/repo/tree/foo/package.json`,
+                    `https://github.com/example/repo/tree/foo/package.json`,
+                    ``,
+                    `other options are ignored`,
+                    `https://github.com/example/repo/tree/v1.4.2/package.json`,
+                    `https://github.com/example/repo/tree/v1.4.2/package.json`,
+                    `https://github.com/example/repo/tree/v1.4.2/package.json`,
+                ].join('\n'),
+            );
+        });
     });
 
     it('template not found', async () => {
