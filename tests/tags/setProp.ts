d901a989819b7b6ed410f13bccca078a92efe9a4
sounisi5011 <sounisi5011@users.noreply.github.com>
2020-06-03 14:27:26 +0900

    ✅ Add test for "setProp" tag

---
+++ tests/tags/setProp.ts
@@ -0,0 +1,79 @@
+import * as path from 'path';
+
+import {
+    createTmpDir,
+    DEFAULT_TEMPLATE_NAME,
+    execCli,
+    readFileAsync,
+    writeFilesAsync,
+} from '../helpers';
+import genWarn from '../helpers/warning-message';
+
+describe('setProp', () => {
+    describe('capture the contents of a block', () => {
+        for (const [id, endBlockName] of Object.entries({
+            'setprop-endset': 'endset',
+            'setprop-endsetprop': 'endsetProp',
+        })) {
+            const name = `setProp to ${endBlockName}`;
+            const idPrefix = `capture-block-contents/${id}`;
+
+            // eslint-disable-next-line jest/valid-title
+            describe(name, () => {
+                it('basic', async () => {
+                    const cwd = await createTmpDir(
+                        __filename,
+                        `${idPrefix}/basic`,
+                    );
+                    await writeFilesAsync(cwd, {
+                        [DEFAULT_TEMPLATE_NAME]: [
+                            `---`,
+                            `foo: {}`,
+                            `---`,
+                            `{%- setProp foo.bar %}hoge{% ${endBlockName} -%}`,
+                            `{{ foo | dump }}`,
+                        ],
+                    });
+
+                    await expect(execCli(cwd, [])).resolves.toMatchObject({
+                        exitCode: 0,
+                        stdout: '',
+                        stderr: genWarn({ pkg: true, pkgLock: true }),
+                    });
+
+                    await expect(
+                        readFileAsync(path.join(cwd, 'README.md'), 'utf8'),
+                    ).resolves.toBe(JSON.stringify({ foo: { bar: 'hoge' } }));
+                });
+
+                it('convert templates in content', async () => {
+                    const cwd = await createTmpDir(
+                        __filename,
+                        `${idPrefix}/convert-template`,
+                    );
+                    await writeFilesAsync(cwd, {
+                        [DEFAULT_TEMPLATE_NAME]: [
+                            `---`,
+                            `foo: {}`,
+                            `---`,
+                            `{%- setProp foo.bar %}hoge{{ 42 }}fuga{% ${endBlockName} -%}`,
+                            `{{ foo | dump }}`,
+                        ],
+                    });
+
+                    await expect(execCli(cwd, [])).resolves.toMatchObject({
+                        exitCode: 0,
+                        stdout: '',
+                        stderr: genWarn({ pkg: true, pkgLock: true }),
+                    });
+
+                    await expect(
+                        readFileAsync(path.join(cwd, 'README.md'), 'utf8'),
+                    ).resolves.toBe(
+                        JSON.stringify({ foo: { bar: 'hoge42fuga' } }),
+                    );
+                });
+            });
+        }
+    });
+});
