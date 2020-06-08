38770efa36e0f39884f19bc12a8d47e25f8a9ea5
sounisi5011 <sounisi5011@users.noreply.github.com>
2020-06-08 23:06:00 +0900

    ⏪ Revert "🔧 Use settings in dprint.config.js file with eslint-plugin-dprint"
    dprint.config.js cannot apply different rules in .js and .ts files.
    However, .eslintrc.* can apply different rules to each file.
    With this in mind, I came up with the idea of not using the dprint CLI, which uses dprint.config.js.
    
    This reverts commit e142496442b216923779dc7466480158f6c6a616.

--- dprint.config.js
+++ dprint.config.js
@@ -1,21 +1,14 @@
 // @ts-check
-
-/** @type { import("dprint-plugin-typescript").TypeScriptConfiguration } */
-exports.tsPluginConfig = {
-    quoteStyle: 'preferSingle',
-};
+const { TypeScriptPlugin } = require("dprint-plugin-typescript");
 
 /** @type { import("dprint").Configuration } */
-exports.config = {
-    projectType: 'openSource',
-    get plugins() {
-        const { TypeScriptPlugin } = require('dprint-plugin-typescript');
-
-        const plugins = [new TypeScriptPlugin(exports.tsPluginConfig)];
-
-        Object.defineProperty(this, 'plugins', { value: plugins });
-        return plugins;
-    },
-    includes: ['**/{,.}*.{ts,js}'],
-    excludes: ['**/coverage/**', 'dist/**', 'tests/**/*.test-result/**'],
+module.exports.config = {
+    projectType: "openSource",
+    plugins: [
+        new TypeScriptPlugin({
+            quoteStyle: "preferSingle",
+        }),
+    ],
+    includes: ["**/{,.}*.{ts,js}"],
+    excludes: ["**/coverage/**", "dist/**", "tests/**/*.test-result/**"],
 };
