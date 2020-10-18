f8b5628581cbd0f4e3694f74385e5a5ea48c033e
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2020-10-18 14:19:59 +0900

    chore(shipjs): fix updete process for `CHANGELOG.md` and `README.md` (#92)
    * chore(shipjs): generate a CHANGELOG have the appropriate headers
    
        Modify the CHANGELOG.md file to keep the header text correct when Ship.js runs the `conventional-changelog`.
    
    * chore(shipjs): update `README.md` when creating PR for release
    
    * chore(debug): add `console.log()` inside the beforeCommitChanges hook of Ship.js
    
        Added debugging code to see why the updated CHANGELOG.md contains the "undefined" string.
    
    * chore(shipjs): fix bug in versionUpdated hook of Ship.js
    
    * revert: chore(debug): add `console.log()` inside the beforeCommitChanges hook of Ship.js

---
+++ ship.config.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const path = require('path');
+
+const hostedGitInfo = require('hosted-git-info');
+
+const pkg = require('./package.json');
+
+const repoURL = typeof pkg.repository === 'string' ? pkg.repository : pkg.repository.url;
+const repoInfo = hostedGitInfo.fromUrl(repoURL);
+
+module.exports = {
+  versionUpdated({ dir }) {
+    // Remove header text in `CHANGELOG.md`
+    const changelogPath = path.resolve(dir, 'CHANGELOG.md');
+    const changelogText = fs.readFileSync(changelogPath, { encoding: 'utf8' });
+    const omitHeaderChangelogText = changelogText.replace(
+      /^\n*(?:# Change Log\n(?:(?!#+ )[^\n]*\n)*)?##? (?:(?:Unreleased|\[Unreleased\](?:\([^)]+\))?)(?: \([0-9-]+\))?)\n(?:(?!#+ )[^\n]*\n)*/,
+      '',
+    );
+    fs.writeFileSync(changelogPath, omitHeaderChangelogText);
+  },
+  beforeCommitChanges({ nextVersion, exec, dir }) {
+    // Add header text in `CHANGELOG.md`
+    const changelogPath = path.resolve(dir, 'CHANGELOG.md');
+    const changelogText = fs.readFileSync(changelogPath, { encoding: 'utf8' });
+    const updatedChangelogText = [
+      `# Change Log`,
+      ``,
+      `## [Unreleased](${repoInfo._fill(`https://{domain}/{user}/{project}/compare/v${nextVersion}...master`)})`,
+      ``,
+      changelogText,
+    ].join('\n');
+    fs.writeFileSync(changelogPath, updatedChangelogText);
+
+    // Update `README.md`
+    exec(`npm run build:readme`);
+  },
+};
